0cc4ccb88226495993dbc4fd725c1edae097f765
Alexander Lagutenko <acbelter@gmail.com>
2017-07-09 13:19:44 +0300

    Добавлен Navigation Drawer

--- app/src/main/java/com/acbelter/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/acbelter/weatherapp/ui/MainActivity.java
@@ -1,7 +1,14 @@
 package com.acbelter.weatherapp.ui;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.acbelter.weatherapp.App;
 import com.acbelter.weatherapp.R;
@@ -18,12 +25,18 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends MvpAppCompatActivity implements MainView {
+public class MainActivity extends MvpAppCompatActivity implements
+        MainView, NavigationView.OnNavigationItemSelectedListener  {
     @Inject
     @InjectPresenter
     MainPresenter mMainPresenter;
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,48 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar,
+                R.string.nav_drawer_open,
+                R.string.nav_drawer_close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_about:
+                break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @ProvidePresenter
