6f898da9961881f5e7c967c97c36500cd7831931
Alexander Lagutenko <acbelter@gmail.com>
2017-07-12 18:29:18 +0300

    Изменена иконка приложения. Векторные иконки вместо растровых. Разметка экрана погоды.

--- app/src/main/java/com/acbelter/weatherapp/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/acbelter/weatherapp/ui/weather/WeatherFragment.java
@@ -1,10 +1,14 @@
 package com.acbelter.weatherapp.ui.weather;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.acbelter.weatherapp.App;
 import com.acbelter.weatherapp.R;
@@ -18,12 +22,28 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import timber.log.Timber;
 
 public class WeatherFragment extends MvpAppCompatFragment implements WeatherView {
     @Inject
     @InjectPresenter
     WeatherPresenter mPresenter;
+    @BindView(R.id.root_layout)
+    ViewGroup mRootLayout;
+    @BindView(R.id.date_text)
+    TextView mDateText;
+    @BindView(R.id.temperature_text)
+    TextView mTemperatureText;
+    @BindView(R.id.units_text)
+    TextView mUnitsText;
+    @BindView(R.id.location_text)
+    TextView mLocationText;
+    @BindView(R.id.weather_image)
+    ImageView mWeatherImage;
+    private Unbinder mUnbinder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +57,7 @@ public class WeatherFragment extends MvpAppCompatFragment implements WeatherView
         super.onDestroyView();
         App.getComponentManager().removeWeatherComponent();
         Timber.d("Remove weather component");
+        mUnbinder.unbind();
     }
 
     @ProvidePresenter
@@ -49,7 +70,21 @@ public class WeatherFragment extends MvpAppCompatFragment implements WeatherView
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        initViews(inflater.getContext());
+
+        return view;
+    }
+
+    private void initViews(Context context) {
+        mRootLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+        mDateText.setText("12 July 2017");
+        mTemperatureText.setText("21");
+        mUnitsText.setText("ºC");
+        mLocationText.setText("Moscow region, Dolgoprudny");
+        mWeatherImage.setImageResource(R.drawable.img_sun);
     }
 
     @Override
