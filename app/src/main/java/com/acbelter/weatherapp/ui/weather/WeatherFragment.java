b52222309417c4e588a1ed227a87330b863c04fb
Alexander Lagutenko <acbelter@gmail.com>
2017-07-13 12:41:11 +0300

    Реализован экран отображения погоды

--- app/src/main/java/com/acbelter/weatherapp/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/acbelter/weatherapp/ui/weather/WeatherFragment.java
@@ -1,12 +1,17 @@
 package com.acbelter.weatherapp.ui.weather;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,10 +19,12 @@ import com.acbelter.weatherapp.App;
 import com.acbelter.weatherapp.R;
 import com.acbelter.weatherapp.domain.model.WeatherData;
 import com.acbelter.weatherapp.presentation.WeatherPresenter;
+import com.acbelter.weatherapp.ui.SimpleAnimationListener;
 import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -26,13 +33,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import timber.log.Timber;
+import xyz.matteobattilana.library.Common.Constants;
 
 public class WeatherFragment extends MvpAppCompatFragment implements WeatherView {
     @Inject
     @InjectPresenter
     WeatherPresenter mPresenter;
-    @BindView(R.id.root_layout)
-    ViewGroup mRootLayout;
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    @BindView(R.id.content_layout)
+    ViewGroup mContentLayout;
     @BindView(R.id.date_text)
     TextView mDateText;
     @BindView(R.id.temperature_text)
@@ -41,15 +51,44 @@ public class WeatherFragment extends MvpAppCompatFragment implements WeatherView
     TextView mUnitsText;
     @BindView(R.id.location_text)
     TextView mLocationText;
+    @BindView(R.id.weather_view)
+    xyz.matteobattilana.library.WeatherView mWeatherView;
     @BindView(R.id.weather_image)
     ImageView mWeatherImage;
     private Unbinder mUnbinder;
+    private WeatherData mWeatherData;
+    private WeatherStateRes mWeatherStateRes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         App.getComponentManager().addWeatherComponent().inject(this);
         super.onCreate(savedInstanceState);
         Timber.d("Add weather component");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Timber.d("Save weather fragment state");
+        if (mWeatherData != null) {
+            outState.putParcelable("weather_data", mWeatherData);
+        }
+        if (mWeatherStateRes != null) {
+            outState.putString("weather_state_res", mWeatherStateRes.name());
+        }
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Timber.d("Restore weather fragment state");
+        if (savedInstanceState != null) {
+            mWeatherData = savedInstanceState.getParcelable("weather_data");
+            String weatherStateResName = savedInstanceState.getString("weather_state_res");
+            if (weatherStateResName != null) {
+                mWeatherStateRes = WeatherStateRes.valueOf(weatherStateResName);
+            }
+        }
     }
 
     @Override
@@ -73,39 +112,164 @@ public class WeatherFragment extends MvpAppCompatFragment implements WeatherView
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
         mUnbinder = ButterKnife.bind(this, view);
 
-        initViews(inflater.getContext());
+        mSwipeRefreshLayout.setOnRefreshListener(() -> {
+            mPresenter.getWeather();
+        });
+
+        mSwipeRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        mWeatherView.setWeather(Constants.weatherStatus.SUN)
+                .setCurrentLifeTime(5000)
+                .setCurrentParticles(15)
+                .setFPS(60)
+                .setCurrentAngle(-10)
+                .setOrientationMode(Constants.orientationStatus.ENABLE);
 
         return view;
     }
 
-    private void initViews(Context context) {
-        mRootLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
-        mDateText.setText("12 July 2017");
-        mTemperatureText.setText("21");
-        mUnitsText.setText("ºC");
-        mLocationText.setText("Moscow region, Dolgoprudny");
-        mWeatherImage.setImageResource(R.drawable.img_sun);
+    private void showWeather(Context context, WeatherData newWeatherData) {
+        // FIXME For testing
+        WeatherStateRes newWeatherStateRes = WeatherUtils.getRandomStateRes();
+        Timber.d("Change weather state res: " + mWeatherStateRes + "->" + newWeatherStateRes);
+
+        int textColor = newWeatherStateRes.getTextColor(context);
+        setWeatherTextColor(textColor);
+
+        mDateText.setText(newWeatherData.date);
+        mTemperatureText.setText(newWeatherData.temperature);
+        mUnitsText.setText(R.string.celsius);
+        mLocationText.setText(newWeatherData.location);
+
+        int colorFrom;
+        int colorTo = newWeatherStateRes.getBackgroundColor(context);
+        final long animBgDuration = context.getResources().getInteger(R.integer.anim_bg_duration);
+
+        WeakReference<xyz.matteobattilana.library.WeatherView> weatherViewWeakRef =
+                new WeakReference<>(mWeatherView);
+        WeakReference<ImageView> weatherImageWeakRef = new WeakReference<>(mWeatherImage);
+        WeakReference<ViewGroup> contentLayoutWeakRef = new WeakReference<>(mContentLayout);
+
+        if (mWeatherData == null || mWeatherStateRes == null) {
+            // Show weather first time
+            colorFrom = ContextCompat.getColor(context, R.color.colorBgWeatherDefault);
+            mWeatherImage.setImageResource(newWeatherStateRes.getWeatherImageResId());
+
+            Animation fadeIn = AnimationUtils.loadAnimation(context, R.anim.anim_fade_in);
+            fadeIn.setAnimationListener(new SimpleAnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    xyz.matteobattilana.library.WeatherView weatherView = weatherViewWeakRef.get();
+                    if (weatherView != null) {
+                        weatherView.setWeather(WeatherUtils.getWeatherStatus(newWeatherStateRes));
+                        weatherView.startAnimation();
+                    }
+                }
+            });
+            mWeatherImage.startAnimation(fadeIn);
+
+            ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+            colorAnimation.setDuration(animBgDuration);
+            colorAnimation.addUpdateListener(animator -> {
+                ViewGroup contentLayout = contentLayoutWeakRef.get();
+                if (contentLayout != null) {
+                    contentLayout.setBackgroundColor((int) animator.getAnimatedValue());
+                }
+            });
+            colorAnimation.start();
+        } else if (mWeatherStateRes != newWeatherStateRes) {
+            // Replace weather
+            Animation fadeOut = AnimationUtils.loadAnimation(context, R.anim.anim_fade_out);
+            Animation fadeIn = AnimationUtils.loadAnimation(context, R.anim.anim_fade_in);
+            fadeOut.setAnimationListener(new SimpleAnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    ImageView weatherImage = weatherImageWeakRef.get();
+                    if (weatherImage != null) {
+                        weatherImage.setImageResource(newWeatherStateRes.getWeatherImageResId());
+                        weatherImage.startAnimation(fadeIn);
+                    }
+                }
+            });
+            fadeIn.setAnimationListener(new SimpleAnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    xyz.matteobattilana.library.WeatherView weatherView = weatherViewWeakRef.get();
+                    if (weatherView != null) {
+                        weatherView.setWeather(WeatherUtils.getWeatherStatus(newWeatherStateRes));
+                        weatherView.startAnimation();
+                    }
+                }
+            });
+            mWeatherImage.startAnimation(fadeOut);
+
+            colorFrom = mWeatherStateRes.getBackgroundColor(context);
+            mWeatherView.cancelAnimation();
+
+            ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+            colorAnimation.setDuration(animBgDuration);
+            colorAnimation.addUpdateListener(animator -> {
+                ViewGroup contentLayout = contentLayoutWeakRef.get();
+                if (contentLayout != null) {
+                    contentLayout.setBackgroundColor((int) animator.getAnimatedValue());
+                }
+            });
+            colorAnimation.start();
+        }
+
+        mWeatherData = newWeatherData;
+        mWeatherStateRes = newWeatherStateRes;
+    }
+
+    private void setWeatherTextColor(int color) {
+        mDateText.setTextColor(color);
+        mTemperatureText.setTextColor(color);
+        mUnitsText.setTextColor(color);
+        mLocationText.setTextColor(color);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if (mWeatherData != null && mWeatherStateRes != null) {
+            mWeatherView.startAnimation();
+        }
+        // FIXME For testing
         mPresenter.getWeather();
     }
 
     @Override
-    public void showWeatherLoading() {
+    public void onPause() {
+        super.onPause();
+        mWeatherView.cancelAnimation();
+    }
 
+    @Override
+    public void showWeatherLoading() {
+        if (mSwipeRefreshLayout.isRefreshing()) {
+            return;
+        }
+        mSwipeRefreshLayout.setRefreshing(true);
     }
 
     @Override
     public void showWeather(List<WeatherData> weatherDataList) {
-
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public void showError() {
-
+        mSwipeRefreshLayout.setRefreshing(false);
+        // FIXME For testing
+        showWeather(getContext(), WeatherData.newDebugInstance());
+//        Snackbar errorSnackbar =
+//                Snackbar.make(mContentLayout, R.string.text_weather_error, Snackbar.LENGTH_LONG);
+//        errorSnackbar.setAction(R.string.ok, v -> errorSnackbar.dismiss());
+//        errorSnackbar.show();
     }
 
     public static String tag() {
--- domain/src/main/java/com/acbelter/weatherapp/domain/model/WeatherData.java
+++ domain/src/main/java/com/acbelter/weatherapp/domain/model/WeatherData.java
@@ -1,4 +1,53 @@
 package com.acbelter.weatherapp.domain.model;
 
-public class WeatherData {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+// FIXME Implementation for testing
+public class WeatherData implements Parcelable {
+    public String date;
+    public String temperature;
+    public String location;
+
+    public static WeatherData newDebugInstance() {
+        WeatherData data = new WeatherData();
+        data.date = "13 July 2017";
+        data.temperature = "31";
+        data.location = "Moscow region";
+        return data;
+    }
+
+    public WeatherData() {
+
+    }
+
+    protected WeatherData(Parcel in) {
+        date = in.readString();
+        temperature = in.readString();
+        location = in.readString();
+    }
+
+    public static final Creator<WeatherData> CREATOR = new Creator<WeatherData>() {
+        @Override
+        public WeatherData createFromParcel(Parcel in) {
+            return new WeatherData(in);
+        }
+
+        @Override
+        public WeatherData[] newArray(int size) {
+            return new WeatherData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(date);
+        dest.writeString(temperature);
+        dest.writeString(location);
+    }
 }
